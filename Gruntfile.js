280816afa93b1fdfa73be8eccd61524f1ef5ce88
Julien Letellier <letellier.julien@gmail.com>
2015-05-04 11:25:45 +0200

    Added uglify and reversed function names back to underscore-style

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,13 +17,22 @@ module.exports = function(grunt) {
                 ],
                 tasks: [ 'browserify:dist' ]
             }
+        },
+
+        uglify: {
+            dist: {
+                files: {
+                    'dist/langeroids.min.js': [ '<%= browserify.dist.dest %>' ]
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-browserify');
 
-    grunt.registerTask('default', [ 'browserify:dist' ]);
+    grunt.registerTask('default', [ 'browserify:dist', 'uglify:dist' ]);
     grunt.registerTask('dev', [ 'browserify:dist', 'watch' ]);
 
 };
